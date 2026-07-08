8a8f772a76bb672ed223521077b0060df675eb9c
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-07-08 18:40:15 -0300

    Load recipe dynamically based on URL hash

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,11 +31,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      //'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8433',
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
     );
     const data = await res.json();
 
@@ -153,3 +157,5 @@ const showRecipe = async function () {
   }
 };
 showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
