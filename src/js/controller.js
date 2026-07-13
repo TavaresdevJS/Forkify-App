25dde566660c1e2683a1156352880e400ce0f2a5
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-07-13 18:49:37 -0300

    Add pagination feature with PaginationView

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -13,9 +14,9 @@ import 'regenerator-runtime/runtime';
 
 ///////////////////////////////////////
 
-if (module.hot) {
+/* if (module.hot) {
   module.hot.accept();
-}
+} */
 
 const controlRecipes = async function () {
   try {
@@ -46,14 +47,26 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
