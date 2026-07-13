88c997f12a73887af27c0958183a40fb5897436a
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-07-13 17:07:28 -0300

    Refactor views to extend shared View class, add ResultsView for search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -11,6 +12,10 @@ import 'regenerator-runtime/runtime';
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -31,6 +36,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -39,7 +46,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
