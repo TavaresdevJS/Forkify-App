be4ffba62deaaf93d1e703876b1bdfb2fca91dcc
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-07-15 18:10:11 -0300

    Add DOM updating Algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,6 +25,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0) Update resuilts view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // 1) Loading recipe
     await model.loadRecipe(id);
 
@@ -68,7 +71,8 @@ const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  //recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
